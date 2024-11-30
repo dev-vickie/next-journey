378f0fdcd8aa54495747ec8211660043267bc102
dev-vickie <vmutethia84@gmail.com>
2024-11-30 11:11:10 +0300

    learn: intro to layouts

---
+++ src/components/PageFooter.tsx
@@ -0,0 +1,15 @@
+export default function PageFooter() {
+  return (
+    <footer
+      style={{
+        padding: "1rem",
+        backgroundColor: "black",
+        color: "white",
+        fontSize: "1.5rem",
+        textAlign: "center",
+      }}
+    >
+      <p>Footer</p>
+    </footer>
+  );
+}
---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,13 @@
+export default function PageHeader() {
+  return (
+    <header style={{
+      padding: '1rem',
+      backgroundColor: 'gray',
+      color: 'white',
+      fontSize: '1.5rem',
+      textAlign: 'center',
+    }}>
+      <h1>Heading</h1>
+    </header>
+  )
+}
